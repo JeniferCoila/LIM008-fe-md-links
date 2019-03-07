19081e26497ffd5f8d51de693a54bd7beed1fdf5
jenifercoila <jeniferfcr@gmail.com>
2019-03-07 12:19:10 -0500

    Agregando funcion de validate links, aun no funciona, pending

--- src/cli.js
+++ src/cli.js
@@ -3,9 +3,5 @@
 
 /* It's like dom, messages like --help can be here too */
 
-import {mdPathContent, isDirectory, filterMdPath, walkTheDirectory} from './utils/util.js';
-
-export const mdlinks = (newpath = relToAbs(path)) => 
-  mdPathContent(filterMdPath(isDirectory(`${newpath}`) ? 
-    walkTheDirectory(`${newpath}`) : [`${newpath}`])).map((content) => 
-    content.match(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm));
+/* import {mdPathContent, isDirectory, filterMdPath, walkTheDirectory} from './utils/util.js';
+ */
--- src/utils/util.js
+++ src/utils/util.js
@@ -3,26 +3,72 @@
 
 import path from 'path';
 import fs from 'fs';
+import fetch from 'node-fetch';
 
-export const isPathAbsolute = (inputPath) => path.isAbsolute(inputPath);
+export const isPathAbsolute = inputPath => path.isAbsolute(inputPath);
+export const unionpath = inputPath => path.join(inputPath);
+
 export const relToAbs = inputPath => path.resolve(inputPath);
 export const dirContent = inputPath => fs.readdirSync(inputPath);
 export const fileNameExt = inputPath => path.extname(inputPath);
-export const content = inputPath => fs.readFileSync(inputPath).toString();
+export const fileContents = inputPath => fs.readFileSync(inputPath).toString();
 export const isDirectory = inputPath => fs.statSync(inputPath).isDirectory();
-export const unionpath = inputPath => path.join(inputPath);
-export const realpathSync = inputPath => fs.realpathSync(inputPath);
+export const regex = /(^|[^!])\[([^\[\]]+)\]\s*\(([^)]+.+)\)/gm;
 
-export const mdPathContent = inputPath => inputPath.map((mdFile) => content(mdFile));
+export const mdPathContent = inputPath => 
+  inputPath.map((mdFile) => getLinks(mdFile));
 
-export const filterMdPath = inputPath => inputPath.filter(file => fileNameExt(file) === '.md');
+export const mdlinks = (newpath = relToAbs(path)) => 
+  mdPathContent(filterMdPath(isDirectory(`${newpath}`) 
+    ? walkTheDirectory(`${newpath}`) 
+    : [`${newpath}`]));
+
+export const getLinks = (path) => {
+  const arrObj = [];
+  const content = fileContents(path);
+  let arrLinks = regex.exec(content);
+  while (arrLinks !== null) {
+    arrObj.push({
+      href: arrLinks[3],
+      text: arrLinks[2],
+      file: path
+    });
+    arrLinks = regex.exec(content);
+  }
+  return arrObj;
+};
+
+export const validateLinks = (path) => {
+  const links = mdlinks(path);  
+  const arrLinks = links.map((link) => fetch(link.href)
+    .then((response) => {
+      if (response.status >= 200 && response.status < 400) {
+        link.status = response.status;
+        link.statusText = response.statusText;
+      } else {
+        link.status = response.status;
+        link.statusText = 'fail';
+      }
+    }));
+  return Promise.all(arrLinks)
+    .then(() => console.log(links))
+    .catch((error) => ({
+      error
+    }));
+};
+
+
+export const filterMdPath = inputPath => 
+  inputPath.filter(file => fileNameExt(file) === '.md');
 
 export const walkTheDirectory = (inputPath) => {
   let arrMdPath = [];
   dirContent(inputPath).forEach((file) => {
-    let newpath = path.join(inputPath, file);
+    let newpath = relToAbs(path.join(inputPath, file));
     (isDirectory(newpath) ? arrMdPath = arrMdPath.concat(walkTheDirectory(newpath)) : arrMdPath.push(newpath));
   });
   return arrMdPath;
 };
+console.log(validateLinks('C:\\Users\\jenif\\OneDrive\\LAB\\PROYECTOS\\First Project\\pruebas\\learnyounode\\program\\olo\\lorem-forus.md'));
+
 
